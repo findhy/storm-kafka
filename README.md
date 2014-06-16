f99cba3261e54831c8553ddf9892f6bcf3029060
findhy <findhyg@gmail.com>
2014-06-16 15:07:04 +0800

    wiki storm

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 storm-kafka
 ===========
+#### 说明 ####
+Storm与Kafka框架集成，依赖https://github.com/wurstmeister/storm-kafka-0.8-plus，参考https://github.com/wurstmeister/storm-kafka-0.8-plus-test修改，包括：
 
-storm-kafka project
+- Kafka的Producer test，通过Websocket读取Wikipedia的实时修改数据
+- Storm的Spout test，订阅Kafka的producer，数据在bolt中处理完成之后再次发送到Kafka中
+
+
+#### 下载 ####
+
+    git clone https://github.com/findhy/storm-kafka.git
+
+#### 打包 ####
+
+    cd storm-kafka
+    mvn clean package -P cluster
+
+#### 提交Kafka Producer ####
+
+
+
+#### 提交Storm Topology ####
+
+
+
+#### 订阅Storm bolt发过来的数据 ####
+
--- src/main/java/org/findhy/storm/kafka/KafkaProperties.java
+++ src/main/java/org/findhy/storm/kafka/KafkaProperties.java
@@ -5,7 +5,14 @@ public interface KafkaProperties {
 	final static String broker_list = "master:9092";
 	final static String partitioner_class = "org.findhy.storm.kafka.partitioner.WikiPartitioner";
 	final static String producer_url = "ws://wikimon.hatnote.com:9000";
-	
+
 	final static String producer_topic = "wikipedia";
 	final static String storm_bolt_topic = "wikipedia-from-storm";
+
+	final static String kafkaServerURL = "master";
+	final static int kafkaServerPort = 9092;
+	final static int kafkaProducerBufferSize = 64 * 1024;
+	final static int connectionTimeOut = 100000;
+	final static int reconnectInterval = 10000;
+	final static int partition_from = 0;
 }
